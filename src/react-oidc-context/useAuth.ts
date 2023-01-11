15917f77e40b5348a1dc3196eae1894036aba03d
Bouhadida Brahim Hamza <bouhadidahz95@hotmail.com>
2023-01-11 14:33:19 +0100

    intercept fetch

--- src/react-oidc-context/useAuth.ts
+++ src/react-oidc-context/useAuth.ts
@@ -1,4 +1,5 @@
 import React from "react";
+import fetchIntercept from "fetch-intercept";
 
 import { AuthContext, AuthContextProps } from "./AuthContext";
 
@@ -10,6 +11,28 @@ export const useAuth = (): AuthContextProps => {
 
     if (!context) {
         throw new Error("AuthProvider context is undefined, please verify you are calling useAuth() as child of a <AuthProvider> component.");
+    }
+
+    if(context?.isAuthenticated)
+    {
+        fetchIntercept.register({
+            request: function(url, defaultConfig) {
+                if(defaultConfig == undefined)
+                {
+                    const config = defaultConfig || {};
+                    config.headers = config.headers || {};
+            
+                    config.headers['Authorization'] = "Bearer " + context.user?.access_token ;
+                    config.headers['Content-Type'] = "application/json" ;
+                    return [url, config];
+            
+                }else{
+                    defaultConfig.headers['Authorization'] = "Bearer " + context.user?.access_token ;
+                    defaultConfig.headers['Content-Type'] = "application/json" ;
+                    return [url, defaultConfig];
+                }
+            }
+        });
     }
 
     return context;
